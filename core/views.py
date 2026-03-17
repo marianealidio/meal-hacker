319a44a8b8fed4be004fc7541fa3a561ea86480d
marianealidio <68953454+marianealidio@users.noreply.github.com>
2026-03-17 11:18:17 +0000

    Update views.py
    Log In

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
 def homepage(request):
     return render(request, "pages/homepage.html")
 def inventory(request):
@@ -11,10 +12,17 @@ def calendar(request):
 def shoppinglist(request):
     return render(request, "pages/shoppinglist.html")
 def profile(request):
-    return render(request, "pages/profile.html") 
-def login(request):
-    return render(request, "pages/login.html")    
+    return render(request, "pages/profile.html")    
 def generatedmeals(request):
     return render(request, "pages/generatedmeals.html") 
+def login_view(request):
+    if request.method == "POST" :
+        email = request.POST.get("email")
+        password = request.POST.get ("password")
+        user = authenticate(request, username = email, password = password)
+        if user is not None: 
+            auth_login(request, user)
+            return redirect("homepage")
+    return render(request, "pages/login.html") 
 def signup(request):
     return render(request, "pages/signup.html") 

46462b48aaf735cacef249bff92daaacb1ab75eb
marianealidio <68953454+marianealidio@users.noreply.github.com>
2026-03-17 18:39:56 +0000

    Update urls.py
    Fixed log in and sign up path and added logout,delete,remove meal

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path("shoppinglist/",views.shoppinglist, name = "shoppinglist"),
     path("profile/",views.profile, name = "profile"),
     path("login/",views.login_view, name = "login"),
-    path("signup/",views.signup, name = "signup"),
+    path("signup/",views.signup_view, name = "signup"),
+    path("logout/",views.logout_view, name = "logout"),
     path("generatedmeals/",views.generatedmeals, name = "generatedmeals"),
+    path("delete-item/<int:item_id>/",views.delete_item_view, name="delete_item"),
+    path("remove-meal/<int:plan_id>/",views.remove_meal_view, name="remove_meal"),
+    path("mark-purchased/<int:item_id>/",views.mark_purchased_view, name="mark_purchased"),
+    path("delete-shopping-item/<int:item_id>/",views.delete_shopping_item_view, name="delete_shopping_item")
     ]

ee0288249e1c2fd585a0d4e60bb1284eb7cb9dbd
marianealidio <68953454+marianealidio@users.noreply.github.com>
2026-04-30 03:05:11 +0100

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import InventoryItem, MealPlan, ShoppingListItem,Reminder
-
-#Allows inventory item, planned meals,shopping list and reminder to be viewed on admin panel
-admin.site.register(InventoryItem)
-admin.site.register(MealPlan)
-admin.site.register(ShoppingListItem)
-admin.site.register(Reminder)
